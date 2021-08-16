698755f9541b9009040d3c885e2ded8dc9f0bca7
hr0016 <hr6480016@gmail.com>
2021-08-16 16:36:24 +0100

    added some feature engineering functions

--- ml_utils/tools.py
+++ ml_utils/tools.py
@@ -22,20 +22,39 @@ def lr_scheduler(epoch, lr):
         return lr * pow(decay_rate, np.floor(epoch / decay_step))
     return lr
 
-# ensure the input is in the shape of (number of in_cycle points, original features) and scaled 
+"""
+ensure the input is in the shape of (number of in_cycle points, original features) and scaled 
+primal feature means unprocessed features - "V", "I", "T"...
+"""
 def cycle_scale_feature_extract(sample_point_gap, in_cycle_data):
     num_points, num_features = in_cycle_data.shape[0], in_cycle_data.shape[1]
+    
+    """output the position of the maximum values of all primal features in the designated cycle"""
+    
     max_point_ind = np.argmax(in_cycle_data, axis=0)
+    
+    """time elapsed till maximum values of all primal features """
     t_to_max = max_point_ind * sample_point_gap
+    
+    """the maximum values of all primal features"""
     max_val = in_cycle_data[max_point_ind,]
+    
     start_point = in_cycle_data[0,:]
     end_point = in_cycle_data[-1,:]
+    
+    """mean values of all primal features"""
     mean_capa = np.mean(in_cycle_data, axis=0)
+    
+    """compute an erea feature (AUC) of all primal features"""
     AUC_est = [0] * num_features
     for m in range(num_points-1):
         AUC_est += (in_cycle_data[m,]+in_cycle_data[m+1,])/2 * sample_point_gap 
     return max_point_ind, t_to_max, max_val, start_point, end_point, mean_capa, AUC_est
 
+
+"""
+a function builds auto-covariance and cross-covariance features for the input in-cycle data points
+"""
 def cov_feature_extract(in_cycle_data):
     n_features = in_cycle_data.shape[1] 
     if n_features == 1:
@@ -48,6 +67,10 @@ def cov_feature_extract(in_cycle_data):
         cm = np.stack(cm, axis=2)
     return cm
 
+"""
+a function segements the in-cycle time-series data uniformly into windows (without overlaps)
+w_size: window size; ws = number of windows constructed
+"""
 def create_window(in_cycle_data, w_size: int):
     num_points, num_features = in_cycle_data.shape[0], in_cycle_data.shape[1]
     std_size = np.round(np.sqrt(num_points))
@@ -59,6 +82,8 @@ def create_window(in_cycle_data, w_size: int):
     w_data = in_cycle_data.reshape((ws, w_size, num_features))
     return w_data
 
+"""a function further reshapes the processed data (after all previous processing including cleaning, interpolation and spliting) into a 4D array with dimension [num_cycles, in_cycle_points, window_size, combinatorics of primal features], which includes new 2D covariance features
+"""
 def cov_data_construct(original_data, w_size):
     in_cycle_points = original_data.shape[1]
     ws = in_cycle_points / w_size
